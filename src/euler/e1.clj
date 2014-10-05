4a9c8c187b8bda1fc09100001162b93d23d97fe9
Carl <carlmhulett@gmail.com>
2014-10-05 19:54:55 -0400

    generalize multiplier function

--- src/euler/e1.clj
+++ src/euler/e1.clj
@@ -5,17 +5,12 @@
 ;user> (reduce + (filter mult3or5? (range 0 1000)))
 ;233168
 
-(defn mult3?
-  "Returns true if input is evenly divisible by 3."
-  [x]
-  (= 0 (mod x 3)))
-
-(defn mult5?
-  "Returns true if input is evenly divisible by 5."
-  [x]
-  (= 0 (mod x 5)))
+(defn mult?
+  "Returns true if input is a multiple of divisor"
+  [input divisor]
+  (= 0 (mod input divisor)))
 
 (defn mult3or5?
   "Returns true if input is a multiple of 3 or 5."
   [x]
-  (or (mult3? x) (mult5? x)))
+  (or (mult? x 3) (mult? x 5)))
