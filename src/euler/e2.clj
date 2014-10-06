736a42028eaf087f3521b6c22ce121b216dd23c0
Carl <carlmhulett@gmail.com>
2014-10-05 21:10:29 -0400

    correct doc strings for e2

--- src/euler/e2.clj
+++ src/euler/e2.clj
@@ -3,13 +3,15 @@
 ;user> (even-sum (max-value-fib-seq nil))
 ;4613732
 
-(defn fib-seq []
+(defn fib-seq
+  "Returns a lazy sequence of fibbonaci values."
+  []
   ((fn rfib [a b]
        (cons a (lazy-seq (rfib b (+ a b)))))
     0 1))
 
 (defn max-value-fib-seq
-  "Returns lazy sequence of fibbonaci values up to max value. Defaults
+  "Returns sequence of fibbonaci values up to max value. Defaults
 to a max of 4,000,000 for nil input."
   [max-value]
   (take-while (partial > (or max-value 4000000)) (fib-seq)))
